7fb56783abb11ed2c98651a51824156921216dce
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2022-05-10 12:01:20 +0900

    reset code file

--- code.js
+++ code.js
@@ -1,65 +1,9 @@
 async function startGame() {
     // FOR HELP, GO TO: README.md
     // 👇 WRITE YOUR CODE BELOW THIS PART 👇
-    // arrowRight.setSizeTo(20);
-    // arrowLeft.setSizeTo(20);
-    // arrowUp.setSizeTo(20);
-    // arrowDown.setSizeTo(20);
-
-    // arrowLeftPlayer.setSizeTo(20);
-    // arrowLeftPlayer.changeEffect('brightness', 50);
-
-    // arrowLeftGoal.setSizeTo(20);
-
-    // forever(async () => {
-    //     arrowLeftPlayer.glide(2, 'arrowLeftGoal');
-    //     arrowLeftPlayer.goTo(arrowLeftPlayer.xPosition, -1000);
-    // })
-
-    let speed = 10;
-
-    forever(() => {
-        if(mouseDown) {
-            dog.glide(.2, 'mouse')
-        }
-
-        if(keyPressed('shift')) {
-            speed = 50;
-        } else {
-            speed = 10;
-        }
-
-        if(keyPressed('right arrow') && keyPressed('down arrow')) {
-            dog.changeXBy(speed)
-            dog.changeYBy(-speed)
-        } else if (keyPressed('right arrow')) {
-            dog.changeXBy(speed)
-        } else if(keyPressed('down arrow')) {
-            dog.changeYBy(-speed)
-        }
-
-        if(keyPressed('up arrow') && keyPressed('left arrow')) {
-            dog.changeXBy(-speed)
-            dog.changeYBy(speed)
-        } else if (keyPressed('left arrow')) {
-            dog.changeXBy(-speed)
-        } else if(keyPressed('up arrow')) {
-            dog.changeYBy(speed)
-        }
-    })
 
 
-    whenPressed('space', () => {
-        dog.element.src = './sprites/cat/costume1.svg'
-    })
-
-
-
-
-
-
-
-
+    dog.goTo(0, 0)
 
 
     // 👆 WRITE YOUR CODE ABOVE THIS PART 👆
